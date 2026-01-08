d45046b9f52628b9ba890963d7fc665b36d1b511
Sagnik Saha <61739808+ScriptKiddie913@users.noreply.github.com>
2026-01-08 15:18:57 +0530

    Create Translate.tsx

---
+++ src/components/Translate.tsx
@@ -0,0 +1,9 @@
+const Translate = () => {
+  return (
+    <div className="fixed top-4 right-4 z-50 bg-white rounded shadow px-2 py-1 text-sm">
+      <div id="google_translate_element"></div>
+    </div>
+  );
+};
+
+export default Translate;
